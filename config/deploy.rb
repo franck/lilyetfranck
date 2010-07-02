96eb6111b2d818f5d176d0c2da24d7c0c1185f82
Franck <franck@franck.(none)>
2010-07-02 14:29:04 +0200

    prepare to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,38 +1,81 @@
-default_run_options[:pty] = true
+# multi-staging
+# command : cap staging deploy
+set :stages, %w(staging production)
+set :default_stage, "production"
 
-# be sure to change these
-set :user, 'barouf'
-set :domain, 'lilyetfranck.com'
-set :application, 'lilyetfranck'
+set :application, "lilyetfranck"
 
-# the rest should be good
-set :repository,  "git@github.com:franck/lilyetfranck.git"
-set :deploy_to, "/home/#{user}/#{domain}"
+set :scm, :git
+set :repository, "git@github.com:franck/#{application}.git"
+set :branch, "master"
 set :deploy_via, :remote_cache
-set :scm, 'git'
-set :branch, 'master'
-set :git_shallow_clone, 1
-set :scm_verbose, true
-set :use_sudo, false
 
-server domain, :app, :web
-role :db, domain, :primary => true
+ssh_options[:port] = 32100
+set :user, "deploy"
+set :admin_runner, "deploy"
+set :git_enable_submodules, true
 
-namespace :deploy do
-  task :restart do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
+set :rake, "/opt/ruby-enterprise/bin/rake"
+
+role :app, "lilyetfranck.com"
+role :web, "lilyetfranck.com"
+role :db, "lilyetfranck.com", :primary => true
+
+task :production do
+  set :deploy_to, "/var/www/#{application}/app"
+  set :env, "production"
+  # Deploy to production site only from stable branch
+  set :branch, "stable"
 end
 
-# namespace :sass do 
-#   desc 'Updates stylesheets if necessary from their Sass templates.' 
-#   task :update => :environment do 
-#     sh "rm -rf public/stylesheets/compiled tmp/sass-cache" 
-#     Sass::Plugin.options[:never_update] = false 
-#     Sass::Plugin.update_stylesheets 
-#   end
-# end
-# 
-# 
-# 
-# after "deploy:symlink", "sass:update"
+task :staging do
+  set :deploy_to, "/var/www/#{application}/staging"
+  set :env, "staging"
+end
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+  
+  desc <<-DESC
+    Run the migrate rake task on the right stage.
+  DESC
+  task :migrate, :roles => :app do
+    run "cd #{current_path}; #{rake} RAILS_ENV=#{env} db:migrate"
+  end
+  
+  desc <<-DESC
+    Create the database for the current RAILS_ENV
+  DESC
+  task :create, :roles => :app do
+    run "cd #{current_path}; #{rake} RAILS_ENV=#{env} db:create"
+  end
+  
+  desc <<-DESC
+    Drop the database for the current RAILS_ENV
+  DESC
+  task :drop, :roles => :app do
+    run "cd #{current_path}; #{rake} RAILS_ENV=#{env} db:drop"
+  end
+  
+  desc <<-DESC
+    Add initial data to database
+  DESC
+  task :bootstrap, :roles => :app do
+    run "cd #{current_path}; #{rake} RAILS_ENV=#{env} merry:bootstrap"
+  end  
+  
+  task :symlink_database, :roles => :app do
+    run "ln -nfs #{release_path}/config/database.example.yml #{release_path}/config/database.yml"
+  end
+  
+end
+after "deploy", "deploy:cleanup"
+after "deploy:update_code", "deploy:symlink_database"
